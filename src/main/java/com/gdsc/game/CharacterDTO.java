fb5d2ca714e827b7f431414717f252fb2a8ee941
ohseongbin <dannybin814@naver.com>
2024-12-26 21:53:37 +0900

    characterDTO

---
+++ src/main/java/com/gdsc/game/CharacterDTO.java
@@ -0,0 +1,20 @@
+package com.gdsc.game;
+
+public class CharacterDTO {
+    private String name;
+    private String job;
+    private Integer level;
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+}
