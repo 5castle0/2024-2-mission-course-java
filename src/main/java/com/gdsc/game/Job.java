a61909145bce6e9759bb61b1deafaefed22e8665
ohseongbin <dannybin814@naver.com>
2024-12-26 19:56:21 +0900

    Job Enum 추가

---
+++ src/main/java/com/gdsc/game/Job.java
@@ -0,0 +1,24 @@
+package com.gdsc.game;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Job {
+    //체력,마나,스킬
+    KNIGHT("Knight",100,0,Arrays.asList("attack,defense")),
+    WIZARD("Wizard",50,50,Arrays.asList("defense,attack2,attack3,attackHard"));
+
+    private String job;
+    private int hp;
+    private int mp;
+    private List<String> skills;
+
+    Job(String job, int hp, int mp, List<String> skills) {
+        this.hp = hp;
+        this.mp = mp;
+        this.skills = skills;
+    }
+
+
+
+}
